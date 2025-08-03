837938e21348a82fbd52eb623633fa68e20c5d6d
Alex Chen <Alex.Chen@wisetechglobal.com>
2025-08-03 19:56:01 +1000

    Implemented initial WPF + Blazor UI infrastructure

---
+++ WiseTorrent.UI/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebView.Wpf;
+using System.Windows;
+
+namespace WiseTorrent.UI
+{
+	public partial class App : Application
+	{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+		public IServiceProvider Services { get; private set; }
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			var serviceCollection = new ServiceCollection();
+			serviceCollection.AddWpfBlazorWebView();
+			// Register your services here
+			Services = serviceCollection.BuildServiceProvider();
+
+			var mainWindow = new MainWindow();
+			mainWindow.Resources.Add(typeof(IServiceProvider), Services);
+			mainWindow.Show();
+		}
+	}
+}
---
+++ WiseTorrent.UI/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Wpf;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WiseTorrent.UI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+		var blazorWebView = this.FindName("BlazorHost") as BlazorWebView;
+		if (blazorWebView != null)
+		{
+			blazorWebView.Services = ((App)Application.Current).Services;
+		}
+
+		Console.WriteLine("MainWindow loaded");
+	}
+}
