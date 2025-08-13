c02b5b52c193f5e1f44ba312683aaa9773b52a92
Alex Chen <Alex.Chen@wisetechglobal.com>
2025-08-13 22:10:57 +1000

    Implemented basics of HTTP tracker client service task logic

--- WiseTorrent.Parsing/Classes/TrackerResponseParser.cs
+++ WiseTorrent.Parsing/Classes/TrackerResponseParser.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WiseTorrent.Parsing.Interfaces;
+using WiseTorrent.Parsing.Types;
+using WiseTorrent.Peers.Types;
 
 namespace WiseTorrent.Parsing.Classes
 {
-	public class TrackerResponseParser
+	public class TrackerResponseParser : ITrackerResponseParser
 	{
+		public TrackerResponse? ParseTrackerResponseFromString(string rawResponse)
+		{
+			var peers = new List<Peer>();
+			return new TrackerResponse(0, peers);
+		}
 	}
 }
---
+++ WiseTorrent.Parsing/Types/TrackerResponse.cs
@@ -0,0 +1,10 @@
+﻿using WiseTorrent.Peers.Types;
+
+namespace WiseTorrent.Parsing.Types
+{
+	public class TrackerResponse(int interval, List<Peer> peers)
+	{
+		public int Interval = interval;
+		public List<Peer> Peers = peers;
+	}
+}
