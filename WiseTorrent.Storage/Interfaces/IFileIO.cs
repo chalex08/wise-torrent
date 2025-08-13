1bf72e03c3bbef247e317dd659bb3803087790f3
ZaneAFK <ztonitto@outlook.com>
2025-08-13 23:01:12 +1000

    Added IO operation methods to IFileIO.cs

--- WiseTorrent.Storage/Interfaces/IFileIO.cs
+++ WiseTorrent.Storage/Interfaces/IFileIO.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace WiseTorrent.Storage.Interfaces
 {
-	public interface IFileIO
+	public interface IFileIO : IDisposable
     {
+
+        // Reads a sequence of bytes from a file asynchronously.
+        // filePath -> File path to read from.
+        // buffer -> Buffer to fill.
+        // offset -> Position in file to start reading.
+        // count -> Number of bytes to read.
+        Task<int> ReadAsync(
+            string filePath,
+            byte[] buffer,
+            long offset,
+            int count,
+            CancellationToken cancellationToken = default);
+
+        // Write a sequence of bytes to a file asynchronously
+        // filePath -> File path to read from.
+        // buffer -> Buffer to fill.
+        // offset -> Position in file to start reading.
+        // count -> Number of bytes to read.
+        Task WriteAsync(
+            string filePath,
+            byte[] buffer,
+            long offset,
+            int count,
+            CancellationToken cancellationToken = default);
+
+        // Delete a file asynchronously
+        Task DeleteAsync(string filePath, CancellationToken cancellationToken = default);
+
+        // Flush buffered data to the disk asynchronously
+        Task FlushAsync(string filePath, CancellationToken cancellationToken = default);
     }
 }
