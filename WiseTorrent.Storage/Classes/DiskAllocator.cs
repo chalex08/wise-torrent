05b13dcd712be4eda4500ad76db4a5c7bff7b8e4
ZaneAFK <ztonitto@outlook.com>
2025-08-17 16:30:42 +1000

    Implemented DiskAllocator

--- WiseTorrent.Storage/Classes/DiskAllocator.cs
+++ WiseTorrent.Storage/Classes/DiskAllocator.cs
@@ -7,7 +7,35 @@ using WiseTorrent.Storage.Interfaces;
 
 namespace WiseTorrent.Storage.Classes
 {
-    internal class DiskAllocator : IDiskAllocator
+    public class DiskAllocator : IDiskAllocator
     {
+        private readonly IFileIO _fileIO;
+
+        public DiskAllocator(IFileIO fileIO)
+        {
+            _fileIO = fileIO;
+        }
+
+        // Allocate disk space for a file
+        public async Task Allocate(string filePath, long fileSize, CancellationToken cancellationToken = default)
+        {
+            if (fileSize < 1)
+                return;
+
+            var buffer = new byte[1];
+            await _fileIO.WriteAsync(filePath, buffer, fileSize - 1, 0, cancellationToken);
+        }
+
+        // Deallocate disk space from a file
+        public async Task Deallocate(string filePath, CancellationToken cancellationToken = default)
+        {
+            await _fileIO.DeleteAsync(filePath, cancellationToken);
+        }
+
+        // Verify disk space allocation for a file
+        public bool VerifyAllocation(string filePath)
+        {
+            return File.Exists(filePath);
+        }
     }
 }
--- WiseTorrent.Storage/Interfaces/IDiskAllocator.cs
+++ WiseTorrent.Storage/Interfaces/IDiskAllocator.cs
@@ -8,5 +8,17 @@ namespace WiseTorrent.Storage.Interfaces
 {
 	public interface IDiskAllocator
 	{
-	}
+        // Allocate disk space for a file
+        // file -> File path to allocate space for
+        // fileSize -> Size of the file to allocate space for
+        Task Allocate(string filePath, long fileSize, CancellationToken cancellationToken = default);
+
+        // Deallocate disk space for a file
+        // filePath -> File path to deallocate space for
+        Task Deallocate(string filePath, CancellationToken cancellationToken = default);
+
+        // Check if a file has allocated disk space
+        // filePath -> File path to check allocation for
+        bool VerifyAllocation(string filePath);
+    }
 }
