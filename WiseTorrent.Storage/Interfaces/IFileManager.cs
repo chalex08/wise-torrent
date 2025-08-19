d895cd0b9d43e1f00b0fcfb12eeea6d0dffa7898
ZaneAFK <ztonitto@outlook.com>
2025-08-19 23:11:33 +1000

    Remove StorageTests.cs file and added missing reference

--- WiseTorrent.Storage/Interfaces/IFileManager.cs
+++ WiseTorrent.Storage/Interfaces/IFileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WiseTorrent.Pieces.Types;
 
 namespace WiseTorrent.Storage.Interfaces
 {
