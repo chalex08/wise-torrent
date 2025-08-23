eaa657cf9aeb709eae90f4a20f797aeca7a152fd
ZaneAFK <ztonitto@outlook.com>
2025-08-23 18:58:58 +1000

    Update IFileManager and naming of ProcessPieceAsync + add FileManager as singleton service

--- WiseTorrent.Storage/Interfaces/IFileManager.cs
+++ WiseTorrent.Storage/Interfaces/IFileManager.cs
@@ -4,17 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WiseTorrent.Pieces.Types;
+using WiseTorrent.Storage.Types;
 
 namespace WiseTorrent.Storage.Interfaces
 {
     public interface IFileManager
     {
-        // Processes piece
-        // This piece is added to a queue which are written to disk after a certain number of pieces are queued
-        // piece -> Piece to process/add to queue
-        public Task ProcessPieceAsync(Piece piece);
-
-        // Flush all queued pieces to disk
-        public Task FlushPiecesAsync();
+        // Write piece to disk using a provided file map
+        public Task WritePieceAsync(Piece piece, FileMap fileMap, CancellationToken cancellationToken);
     }
 }
