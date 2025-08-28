78c5da7d11bc098c7dc53c09c6aa1939006bbb91
ZaneAFK <ztonitto@outlook.com>
2025-08-28 18:06:17 +1000

    Updated IFileManager

--- WiseTorrent.Storage/Interfaces/IFileManager.cs
+++ WiseTorrent.Storage/Interfaces/IFileManager.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WiseTorrent.Pieces.Types;
-using WiseTorrent.Storage.Types;
+using WiseTorrent.Utilities.Types;
 
 namespace WiseTorrent.Storage.Interfaces
 {
     public interface IFileManager
     {
-        // Write piece to disk using a provided file map
-        public Task WritePieceAsync(Piece piece, FileMap fileMap, CancellationToken cancellationToken);
+        // Write block to disk using a provided file map
+        public Task WriteBlockAsync(Block block, FileMap fileMap, CancellationToken cancellationToken);
     }
 }
