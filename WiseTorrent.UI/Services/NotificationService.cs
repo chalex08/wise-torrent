835081cc9d72f4ae94d41f8d2fb9ef160ebd0d4e
ZaneAFK <ztonitto@outlook.com>
2025-08-31 23:07:46 +1000

    Created toast/notification system + styling

---
+++ WiseTorrent.UI/Services/NotificationService.cs
@@ -0,0 +1,17 @@
+﻿public class NotificationService
+{
+	public event Action<string, NotificationType>? OnNotify;
+
+	public void Show(string message, NotificationType type = NotificationType.Success)
+	{
+		OnNotify?.Invoke(message, type);
+	}
+}
+
+public enum NotificationType
+{
+	Success,
+	Warning,
+	Error,
+	Info
+}
